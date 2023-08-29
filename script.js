6c7a8b54b0db1615c9638944eb0758ba5a97bc71
kushagra1934 <128407478+kushagra1934@users.noreply.github.com>
2023-08-29 15:15:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ var tl=gsap.timeline({
     scrollTrigger:{
         trigger:".page1 h1",
         scroller:".main",
-        markers:true,
+        
         start:"top 27%",
         end:"top 0%",
         scrub:3
@@ -67,7 +67,7 @@ var tl2=gsap.timeline({
   scrollTrigger:{
       trigger:".page1 h1",
       scroller:".main",
-      markers:true,
+      
       start:"top -115%",
       end:"top -120%",
       scrub:3
@@ -82,7 +82,7 @@ var tl3=gsap.timeline({
   scrollTrigger:{
       trigger:".page1 h1",
       scroller:".main",
-      markers:true,
+      
       start:"top -280%",
       end:"top -300%",
       scrub:3
